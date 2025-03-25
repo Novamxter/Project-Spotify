a3327e7c076ff0bff848dbaa023df7aa170ee99b
Novamxter <mehramkmohit968@gmail.com>
2025-03-25 12:45:24 +0530

    24 March Updated

---
+++ Scripts/searchCard.js
@@ -0,0 +1,7 @@
+function renderSearchCards(){
+  let html = `
+  <div class="search-card">
+    <p class="sc-title">Music</p>
+    <img src="./assets/Images/search_cate/sp-01.jpeg" alt="" class="sc-img">
+  </div>`
+}

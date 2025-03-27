54209321c66d3c16c6eabe45d407ebbf5818f48f
Novamxter <mehramkmohit968@gmail.com>
2025-03-27 12:47:08 +0530

    Navbar almost done

--- Scripts/searchCard.js
+++ Scripts/searchCard.js
@@ -2,19 +2,18 @@ export async function addCards() {
   try {
     let response = await fetch("../JSON_Data/search_cards.json");
     let data = await response.json();
-    data.forEach(card=>{
-      createCard(card)
-    })
+    data.forEach((card) => {
+      createCard(card);
+    });
   } catch (e) {
-    console.log("JSON data not fetched ..",e);
+    console.log("JSON data not fetched ..", e);
   }
 }
 function createCard(card) {
-  let div = document.createElement("div")
-  div.className = "search-card"
-  div.style.backgroundColor = card.bgColor
-  div.innerHTML = 
-   `<p class="sc-title">${card.title}</p>
+  let div = document.createElement("div");
+  div.className = "search-card";
+  div.style.backgroundColor = card.bgColor;
+  div.innerHTML = `<p class="sc-title">${card.title}</p>
     <img src="${card.imgSrc}" alt="" class="sc-img">`;
-    document.querySelector(".search-card-container").appendChild(div)
+  document.querySelector(".search-card-container").appendChild(div);
 }
